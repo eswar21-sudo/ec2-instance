072d3ff7a08dc4abb06475a5103332a2b3d66aa4
eswar21-sudo <eswarkumar5475@gmail.com>
2025-07-18 14:10:02 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,37 @@
+resource "aws_security_group" "ec2_sg" {
+  name        = "${var.name}-sg"
+  description = "Security group for EC2 instance"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description = "Allow SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow all egress"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = var.tags
+}
+
+resource "aws_instance" "this" {
+  ami                         = var.ami_id
+  instance_type               = var.instance_type
+  subnet_id                   = var.subnet_id
+  vpc_security_group_ids      = [aws_security_group.ec2_sg.id]
+  associate_public_ip_address = var.associate_public_ip
+
+  key_name = var.key_name
+
+  tags = merge(var.tags, {
+    Name = var.name
+  })
+}
