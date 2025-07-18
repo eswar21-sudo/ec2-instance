cc056e3dd5a36cacf6e22963f8a430aafc854dce
eswar21-sudo <eswarkumar5475@gmail.com>
2025-07-18 14:10:32 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,42 @@
+variable "name" {
+  description = "Name of the EC2 instance"
+  type        = string
+}
+
+variable "ami_id" {
+  description = "AMI ID to use for the instance"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "subnet_id" {
+  description = "Subnet ID to launch the instance in"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "VPC ID for the security group"
+  type        = string
+}
+
+variable "key_name" {
+  description = "Key pair name to SSH into the instance"
+  type        = string
+}
+
+variable "associate_public_ip" {
+  description = "Whether to associate a public IP address"
+  type        = bool
+  default     = true
+}
+
+variable "tags" {
+  description = "A map of tags to assign to resources"
+  type        = map(string)
+  default     = {}
+}
